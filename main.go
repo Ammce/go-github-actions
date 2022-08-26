4c447a7fab84e5569d3e0dee56978aabd58c9d7b
Amel Muminovic <amcenp@gmail.com>
2022-08-26 22:40:47 +0200

    test commit

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello there from Golang")
+	result := SumTwoNumbers(10, 12)
+	fmt.Println("Hello there from Golang", result)
+
+}
+
+func SumTwoNumbers(a int32, b int32) int32 {
+	return a + b
 }
